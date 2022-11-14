355288de0ebd28c6f7f32ad621ee8aca2387ea83
Hanh Duong <dg.hanh117@gmail.com>
2022-11-14 13:58:13 +0200

    push ci-cd.yaml file to GitHub

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Funding Coding/i);
+  const linkElement = screen.getByText(/Registration Form/i);
   expect(linkElement).toBeInTheDocument();
 });
