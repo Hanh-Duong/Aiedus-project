e98ef6d2e78ef477f32da2dbd738a52f8cf14377
Hanh Duong <dg.hanh117@gmail.com>
2022-11-14 13:19:38 +0200

    change add to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Javascript/i);
+  const linkElement = screen.getByText(/Funding Coding/i);
   expect(linkElement).toBeInTheDocument();
 });
